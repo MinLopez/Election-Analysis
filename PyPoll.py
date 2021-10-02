44e08268ada52ecbd5e425ccda6664024e62235d
MinLopez <mmaga006@gmail.com>
2021-10-02 00:10:21 -0700

    adding files with header

--- PyPoll.py
+++ PyPoll.py
@@ -2,15 +2,29 @@ import csv
 import os
 #Add file name variable that references path to file
 file_to_load = os.path.join("Resources", "election_results.csv")
-
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 # Assign a variable for the file to load and the path.
-#file_to_load='C:\Workspace\Election-Analysis\Resources\election_results.csv'
+# file_to_load='C:\Workspace\Election-Analysis\Resources\election_results.csv'
 
 
 #Open Election File
 with open(file_to_load) as election_data:
     #To do: Perform Analysis.
-    print(election_data)
+     # Read the file object with the reader function.
+    file_reader = csv.reader(election_data) 
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
+
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+   # Write three counties to the file.
+     txt_file.write("Counties in the Election\n----------------------------\nArapahoe\nDenver\nJefferson")
+
+
 # Total number of votes cast
 # A complete list of candidates who received votes
 # Total number of votes each candidate received
